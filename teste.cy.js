d74d072eb6bd7db727ceb2a709998189c9d56804
[200~Vinicius06Carlosgit config --global user.name Vinicius06Carlosgit config --global user.name [200~Vinicius06Carlos <viniciuscarlos8258@uni9.edu.br>
2023-06-17 19:01:45 -0300

    Projeto Tinnova

---
+++ teste.cy.js
@@ -0,0 +1,64 @@
+
+/// <reference types="cypress" />
+
+const { should } = require("chai");
+
+describe('Validar testes', () => {
+
+
+
+
+    it('Deve clicar no botão', () => {
+        cy.visit('https://tinnova-teste-qa.vercel.app/');
+
+
+        cy.contains('Entrar').click();
+        cy.get('.sc-hKMtZM > :nth-child(3)').click()
+
+        // add Cliente
+        // Nome
+        cy.get(':nth-child(2) > .sc-iqcoie').type('ViniciusCarlos')
+        // e-mail
+        cy.get(':nth-child(3) > .sc-iqcoie').type('vinicius@gmail.com')
+        // CPF
+        cy.get(':nth-child(4) > .sc-iqcoie').type('40568751892')
+        // Telefone\\
+        cy.get(':nth-child(5) > .sc-iqcoie').type('11997050964')
+        cy.get('.sc-bczRLJ').click()
+
+
+        // consultar usuario 
+
+        cy.get(':nth-child(3) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(2) > svg').click()
+       cy.get(':nth-child(2) > .sc-iqcoie').should('eq', 'Vinicius Carlos')
+        cy.get('.sc-bczRLJ').click()
+
+
+
+        // Editar 
+        cy.get(':nth-child(1) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(3) > svg').click()
+        // Nome
+        cy.get('input[name=name]').type('Maria Lucia')
+
+        // Atualizar
+       cy.get('.sc-bczRLJ').click()
+        
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+    });
+
+
+
+
+});
