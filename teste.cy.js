eec61877530583952f83490675e2ae938bcd165e
Vinicius06Carlos <125702702+Vinicius06Carlos@users.noreply.github.com>
2023-07-07 12:11:11 -0300

    Update teste.cy.js

--- teste.cy.js
+++ teste.cy.js
@@ -1,14 +1,9 @@
 
 /// <reference types="cypress" />
 
-// teste Vinicius
 
-describe('Validar testes', () => {
-
-
-
-
-    it('Deve clicar no botão', () => {
+describe('Validar teste', () => {
+    it('Cadastrar cliente', () => {
         cy.visit('https://tinnova-teste-qa.vercel.app/');
 
 
@@ -23,40 +18,55 @@ describe('Validar testes', () => {
         // CPF
         cy.get(':nth-child(4) > .sc-iqcoie').type('40568751892')
         // Telefone\\
-        cy.get(':nth-child(5) > .sc-iqcoie').type('11997050964')
-        cy.get('.sc-bczRLJ').click()
+        cy.get(':nth-child(5) > .sc-iqcoie').type('11994877322')
+        cy.get('.sc-bczRLJ').click();
+
+    });
 
 
-        // consultar usuario 
-
-        cy.get(':nth-child(3) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(2) > svg').click()
-        cy.get(':nth-child(2) > .sc-iqcoie').should('eq', 'Vinicius Carlos')
-        cy.get('.sc-bczRLJ').click()
-
-
-
-        // Editar 
-        cy.get(':nth-child(1) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(3) > svg').click()
-        // Nome
-        cy.get('input[name=name]').type('Maria Lucia')
-
-        // Atualizar
-       cy.get('.sc-bczRLJ').click()
+      it('Especionar cliente', () => {
+        cy.visit('https://tinnova-teste-qa.vercel.app/');
         
+        cy.contains('Entrar')
+        .click();
+        cy.get(':nth-child(2) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(2)')
+        .click()
+        cy.get('.sc-bczRLJ').click()
 
+    });
 
-
- 
-
-
-
-
-
-
-
+    it('Editar', () => {
+        cy.visit('https://tinnova-teste-qa.vercel.app/');
+        
+        cy.contains('Entrar').click();
+        cy.get(':nth-child(1) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(3)')
+        .click();
+        // Editar Nome
+        cy.get(':nth-child(2) > .sc-iqcoie')
+        .clear();
+        cy.get(':nth-child(2) > .sc-iqcoie').
+        type('João Souza');
+        // Editar e-mail
+        cy.get(':nth-child(3) > .sc-iqcoie').clear();
+        cy.get(':nth-child(3) > .sc-iqcoie').type('joao@joaosilva.com')
+        // Atualizar
+        cy.get('.sc-bczRLJ').click();
 
 
     });
+
+    it('Excluir', () => {
+        cy.visit('https://tinnova-teste-qa.vercel.app/');
+
+        cy.contains('Entrar').click();
+        cy.get(':nth-child(1) > [style="width: 15%; justify-content: flex-start;"] > :nth-child(4) > svg')
+        .click();
+        cy.get(':nth-child(2) > .swal-button').click();
+
+    });
+
+
+
 
 
 
